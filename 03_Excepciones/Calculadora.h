2c66578a9583032a5c8818c409f1b4afecf82a3a
Ignacio <nachobancher@gmail.com>
2017-08-09 10:40:15 -0300

    Se realizo el ejercicio 3

--- 03_Excepciones/Calculadora.h
+++ 03_Excepciones/Calculadora.h
@@ -1,10 +1,45 @@
 #ifndef REPASO_CALCULADORA_H
 #define REPASO_CALCULADORA_H
 
+template class <class T>
 
 class Calculadora {
+private:
+    T A;
+public:
+    const T &getA() const {
+        return A;
+    }
 
+    void setA(const T &A) {
+        Calculadora::A = A;
+    }
+
+private:
+    T B;
+public:
+    const T &getB() const {
+        return B;
+    }
+
+    void setB(const T &B) {
+        Calculadora::B = B;
+    }
 };
+public:
+T sumar(){
+    return A + B;
+}
+T restar(){
+    return A - B;
+}T multiplicar(){
+    return A * B;
+}T dividir(){
+    if(B==0);
+       throw 3;
+    return A/B;
 
+}
+};
 
 #endif //REPASO_CALCULADORA_H
--- 03_Excepciones/main.cpp
+++ 03_Excepciones/main.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main() {
+    Calculadora<int >calc;
+    calc.setA(300);
+    cal.setB(0);
+    try{
+      cout<<calc.dividir()<<endl;
+
+    }catch(int e){
+        cout<<"B no puede ser 0: "<<e<<endl;
+    }
+
     cout << "Ejercicio 00/03\n" << endl;
     return 0;
 }
