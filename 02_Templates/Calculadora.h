15095125595fdf80fbe84706d80c722f15e867dc
Ignacio <nachobancher@gmail.com>
2017-08-09 10:32:33 -0300

    Se realizo el ejercicio 2

--- 02_Templates/Calculadora.h
+++ 02_Templates/Calculadora.h
@@ -1,9 +1,40 @@
 #ifndef REPASO_CALCULADORA_H
 #define REPASO_CALCULADORA_H
 
+template class <class T>
 
 class Calculadora {
+private:
+    T A;
+    T B;
+public:
+    const T &getA() const {
+        return A;
+    }
 
+public:
+    void setB(const T &B) {
+        Calculadora::B = B;
+    }
+
+    void setA(const T &A);
+
+public:
+    const T &getB() const;
+
+}
+
+public:
+    T sumar(){
+        return A + B;
+    }
+    T restar(){
+        return A - B;
+    }T multiplicar(){
+        return A * B;
+    }T dividir(){
+        return A / B;
+    }
 };
 
 
--- 02_Templates/main.cpp
+++ 02_Templates/main.cpp
@@ -4,6 +4,30 @@
 using namespace std;
 
 int main() {
+    Calculadora<int>calInt;
+    Calculadora<char>calChar;
+    Calculadora<float>calFloat;
+    Calculadora<string> calstring;
+
+   //Calculadora de Ints
+    calInt.setA(4);
+    calInt.setB(8);
+    cout<<calInt.sumar()<<endl;
+    cout<<calInt.dividir()<<endl<<endl;
+    //Calculadora de chars
+    calchar.setA(4);
+    calchar.setB(5);
+    cout<<calchar.sumar()<<endl;
+    cout<<calchar.dividir()<<endl<<endl;
+    //Calculadora de float
+    calfloat.setA(4);
+    calfloat.setB(5);
+    cout<<calfloat.sumar ()<<endl;
+    cout<<calfloat.dividir()<<endl<<endl;
+    //Calculadora de string
+    calstring.setA("Hola");
+    calstring.setB("Mundo");
+    cout<<calstring.sumar ()<<endl;
     cout << "Ejercicio 00/02\n" << endl;
     return 0;
 }
